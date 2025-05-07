767272b4ed1b2f9899d668380e24f49672ac7596
lere dakshak mamman <dshak0672@gmail.com>
2025-05-07 09:26:49 +0000

    feat: Home page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Home from './components/home'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
+    <Home/>
+
     </div>
   )
 }
